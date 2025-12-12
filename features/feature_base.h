84dd7a48217ce48bc07f51d3ce2af5d9d7119565
Edirent <sz73@illinois.edu>
2025-12-12 16:14:35 +0400

    base/

---
+++ features/feature_base.h
@@ -0,0 +1,33 @@
+// feature_base.h
+
+#pragma once
+#include <cstdint>
+#include <string>   
+#include <limits>
+#include "feature_spec.h"
+
+namespace feat {
+
+struct BookTopEvent
+{
+    int64_t ts_ns = 0;
+    double bid_px = 0.0;
+    double bid_qty = 0.0;
+    double ask_px = 0.0;
+    double ask_qty = 0.0;
+};
+
+class IFeature{
+public:
+    virtual ~IFeature() = default;
+    virtural void update(const BookTopEvent &event) = 0;
+    virtual void reset() = 0;
+    virtual bool ready() const = 0;
+    virtual double value() const = 0;
+    virtual const FeatureSpec& spec() const = 0;
+protected:
+    static double NaN() {
+        return std::numeric_limits<double>::quiet_NaN();
+    }
+};
+}  // namespace feat
